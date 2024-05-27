a87bf76934ed0305ecd85de38ea035edd3c510bd
kavghade <kavghade@asurint.com>
2024-05-27 10:10:59 +0530

    init

---
+++ api/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from api.models import User
+from django.contrib.auth.admin import UserAdmin
+
+class UserAdmin(UserAdmin):
+    list_display = ['email', 'name', 'is_admin']
+    list_filter = ['is_admin']
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        ("Personal info", {"fields": ["name"]}),
+        ("Permissions", {"fields": ["is_admin"]}),
+    ]
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "name", "password1", "password2"],
+            },
+        ),
+    ]
+    search_fields = ["email"]
+    ordering = ["email"]
+    filter_horizontal = []
+
+admin.site.register(User, UserAdmin)
+# ... and, since we're not using Django's built-in permissions,
+# unregister the Group model from admin.
+# admin.site.unregister(Group)
---
+++ api/views.py
@@ -0,0 +1,29 @@
+from rest_framework.response import Response 
+from rest_framework import status 
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
+from api.serializers import UserRegistrationSerializer, UserLoginSerializer
+
+class UserRegistrationView(APIView):
+
+    def post(self, request, format=None):
+        serializer = UserRegistrationSerializer(data = request.data)
+        if serializer.is_valid(raise_exception = True) :
+            user = serializer.save()
+            return Response("ok", status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+class UserLoginView(APIView):
+    
+    def post(self, request, format=None):
+        serializer = UserLoginSerializer(data = request.data)
+        if serializer.is_valid() :
+            email = serializer.data.get('email')
+            password = serializer.data.get('password')
+            user = authenticate(email=email, password=password)
+            if user is not None: 
+                return Response({'msg':'Login Success'}, status = status.HTTP_200_OK)
+            else : 
+                return Response({'errors': {'non_field_errors':["Email or Password is not valid"]}}, status = status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
