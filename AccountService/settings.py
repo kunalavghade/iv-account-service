a27dd47b6b71b918d431ed4bcd382836db30f159
kunalavghade <kunalavghade@gmail.com>
2024-08-03 20:07:49 +0530

    correction

--- AccountService/settings.py
+++ AccountService/settings.py
@@ -160,6 +160,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EAMIL_PORT=587
-EMAIL_HOST_USER=os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
 
